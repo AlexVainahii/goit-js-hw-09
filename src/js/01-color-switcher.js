ff0a5255aacf691df40c37653d75eb8c62d523b8
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-09-29 23:40:43 +0300

    task 1 final

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+class SwitchBodyColor {
+  constructor() {
+    this.bodyRef = document.querySelector('body');
+    this.btnStartRef = document.querySelector('[data-start]');
+    this.btnStopRef = document.querySelector('[data-stop]');
+    this.timerId = null;
+  }
 
+  GetRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  SwitchColor() {
+    this.bodyRef.style.backgroundColor = this.GetRandomHexColor();
+  }
+  btnStartToggleDis() {
+    this.btnStartRef.disabled = this.btnStartRef.disabled ? false : true;
+  }
+  Listener() {
+    this.btnStartRef.addEventListener('click', () => {
+      this.SwitchColor();
+      this.btnStartToggleDis();
+      this.timerId = setInterval(() => {
+        this.SwitchColor();
+      }, 1000);
+    });
+
+    this.btnStopRef.addEventListener('click', () => {
+      clearInterval(this.timerId);
+      this.btnStartToggleDis();
+    });
+  }
+  Init() {
+    this.Listener();
+  }
+}
+
+const bodyColor = new SwitchBodyColor();
+bodyColor.Init();
