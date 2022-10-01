5b2d674c68e5feff242bb7cf217d256bbd2b695a
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-10-01 09:30:10 +0300

    timer task final

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,10 +1,14 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
-let dateNow = new Date();
 let dateFuture = new Date();
 let timerId = null;
-let timerId1 = null;
+const inDataTimeRef = document.querySelector('#datetime-picker');
+const daysSpanRef = document.querySelector('[data-days]');
+const hoursSpanRef = document.querySelector('[data-hours]');
+const minutesSpanRef = document.querySelector('[data-minutes]');
+const secondsSpanRef = document.querySelector('[data-seconds]');
+const btnStartRef = document.querySelector('[data-start]');
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -23,19 +27,20 @@ function btnStartToggleDis(bool) {
   btnStartRef.disabled = bool ? false : true;
 }
 function timerStartSettings() {
-  btnStartToggleDis(true)
-  clearInterval(timerId)
+  btnStartToggleDis(true);
+  clearInterval(timerId);
   dataSet({});
 }
-function endTime(){
-return dateFuture<=new Date();
+function endTime() {
+  return dateFuture <= new Date();
 }
 function makeMessage(bool) {
-  if (bool) 
-    {timerStartSettings()}
-    else
-    { Notiflix.Notify.failure('Please choose a date in the future');
-    btnStartToggleDis(false)
+  if (bool) {
+    timerStartSettings();
+  } else {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    timerStartSettings();
+    btnStartToggleDis(false);
   }
 }
 
@@ -50,34 +55,32 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   return { days, hours, minutes, seconds };
 }
-function addLeadingZero(value=""){
-  return value.padStart(2,"0")
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 }
-function dataSet({ days="00", hours="00", minutes="00", seconds="00" }) {
-  daysSpanRef.textContent = days;
-  hoursSpanRef.textContent = hours;
-  minutesSpanRef.textContent = minutes;
-  secondsSpanRef.textContent = seconds;
+function dataSet({
+  days = '00',
+  hours = '00',
+  minutes = '00',
+  seconds = '00',
+}) {
+  daysSpanRef.textContent = addLeadingZero(days);
+  hoursSpanRef.textContent = addLeadingZero(hours);
+  minutesSpanRef.textContent = addLeadingZero(minutes);
+  secondsSpanRef.textContent = addLeadingZero(seconds);
 }
-
-const inDataTimeRef = document.querySelector('#datetime-picker');
-const daysSpanRef = document.querySelector('[data-days]');
-const hoursSpanRef = document.querySelector('[data-hours]');
-const minutesSpanRef = document.querySelector('[data-minutes]');
-const secondsSpanRef = document.querySelector('[data-seconds]');
-const btnStartRef = document.querySelector('[data-start]');
 
 flatpickr(inDataTimeRef, options);
 btnStartToggleDis(false);
 
-
 btnStartRef.addEventListener('click', () => {
   dataSet(dateDiff());
   timerId = setInterval(() => {
-
-   if (endTime()) {timerStartSettings()
-    Notiflix.Notify.success('Timer end');
-  }
-   else {dataSet(dateDiff());}
+    if (endTime()) {
+      timerStartSettings();
+      Notiflix.Notify.success('Timer  end');
+    } else {
+      dataSet(dateDiff());
+    }
   }, 1000);
 });
